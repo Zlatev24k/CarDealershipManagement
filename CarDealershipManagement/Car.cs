2754cf4b70a393082bc0978b790320a490dd6d24
Берк Билял <B.Bilyal19a@pmgrz.net>
2024-07-05 11:47:38 +0300

    i added properties in Car

--- CarDealershipManagement/Car.cs
+++ CarDealershipManagement/Car.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management.Instrumentation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,11 @@ namespace CarDealershipManagement
 {
     internal class Car
     {
-
+        public string CarID {  get;private set; }
+        public string Brand {  get;private set; }
+        public string Model {  get;private set; }
+        public int Year { get;private set; }
+        public double Price { get;private set; }
+        public bool Available { get;private set; }
     }
 }
--- CarDealershipManagement/Program.cs
+++ CarDealershipManagement/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CarDealershipManagement
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
