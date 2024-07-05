b9ff8eb58ac1f302f5eaf9bd246ded2108b9f66b
Берк Билял <B.Bilyal19a@pmgrz.net>
2024-07-05 11:33:53 +0300

    Add project files.

---
+++ CarDealershipManagement/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealershipManagement
+{
+    internal class Car
+    {
+
+    }
+}
